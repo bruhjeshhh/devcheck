157de4a12606727a50952cfd2ae1771d4cd335bb
Vidya Sagar Reddy Desu <35026133+vidya381@users.noreply.github.com>
2026-02-28 20:44:15 -0600

    make DockerDaemonCheck runner injectable for testing

--- internal/check/docker.go
+++ internal/check/docker.go
@@ -5,15 +5,23 @@ import (
 	"os/exec"
 )
 
-type DockerDaemonCheck struct{}
+type DockerDaemonCheck struct {
+	runner func() error
+}
 
 func (c *DockerDaemonCheck) Name() string {
 	return "Docker daemon running"
 }
 
 func (c *DockerDaemonCheck) Run(_ context.Context) Result {
-	err := exec.Command("docker", "info").Run()
-	if err != nil {
+	run := c.runner
+	if run == nil {
+		run = func() error {
+			return exec.Command("docker", "info").Run()
+		}
+	}
+
+	if err := run(); err != nil {
 		return Result{
 			Name:    c.Name(),
 			Status:  StatusFail,
